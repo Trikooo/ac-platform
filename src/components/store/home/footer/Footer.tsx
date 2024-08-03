f7b9bc21b064d466d257339ffec35309a14dbd8f
trikooplays <trikooplays@gmail.com>
2024-08-03 11:14:46 +0100

    fixed seo

--- src/components/store/home/footer/Footer.tsx
+++ src/components/store/home/footer/Footer.tsx
@@ -5,11 +5,11 @@ export default function Footer() {
     <footer className="gap-5 mt-24">
       <hr className="border-t border-gray-200 my-8 mx-24" />
       <nav className="flex justify-center items-center gap-12 text-muted-foreground">
-        <a>About</a>
-        <a>Jobs</a>
-        <a>Accessibility</a>
-        <a>Partners</a>
-        <a>Press</a>
+        <a href="#">About</a>
+        <a href="#">Jobs</a>
+        <a href="#">Accessibility</a>
+        <a href="#">Partners</a>
+        <a href="#">Press</a>
       </nav>
       <div className="text-center mt-8 text-muted-foreground">
         &copy; {currentYear} Kotek. All rights reserved.
