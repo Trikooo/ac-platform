626e2cd8858ed94510fa2138cedb0df7656a14c7
trikooplays <trikooplays@gmail.com>
2025-01-08 08:32:07 +0100

    typo fix

--- src/app/admin/settings/page.tsx
+++ src/app/admin/settings/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import AdminLayout from "../AdminLayout";
-import CarouselSettings from "./components/carouselPage";
-
+import CarouselSettings from "./components/CarouselPage";
 
 export default function Settings() {
   return (
